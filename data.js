4b7972ad4905a54c7b7f9cca5c0251672ae3b6e1
dark-shadowblade <150048154+dark-shadowblade@users.noreply.github.com>
2025-03-25 12:21:28 +0530

    Update data.js

--- data.js
+++ data.js
@@ -6,8 +6,11 @@ const subjectsData = [
                 name: "Unit 1: Algebra",
                 notes: "https://example.com/algebra_notes.pdf",
                 lectures: [
-                    "https://u.pcloud.link/publink/show?code=XZ9LU55Zh3Spos9PjPSbRNtomDREFkoW9gQy",
-                    "https://u.pcloud.link/publink/show?code=XU2MPZPZfGJyRVEQr4RnH8ZD56V7GZ1VX5V",
+                    "https://u.pcloud.link/publink/show?code=XZkLd85ZORcK6nR8s2XHcz2p0RC16RQR5vJk",
+                    "https://u.pcloud.link/publink/show?code=XZnLd85ZC3eIx9QC3vYKzHzKgaOh6yRSnsp7",
+                    "https://u.pcloud.link/publink/show?code=XZ2yq85ZmW1xrCA93i8M9WsqAh25hV0NFzN7",
+                    "https://u.pcloud.link/publink/show?code=XZLHq85ZoE6sNC6i7YfcAaSO0MQ35XoIRoXk"
+                    "https://u.pcloud.link/publink/show?code=XZ4Hq85ZM2E2i3SePmSvvi8SjUwfKYNIKDLV",
                 ],
                 dpps: [
                     "https://example.com/algebra_dpp1.pdf",
