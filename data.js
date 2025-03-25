d3d811a4526bad19da454c8e06ae7b48ecd55780
dark-shadowblade <150048154+dark-shadowblade@users.noreply.github.com>
2025-03-25 12:29:39 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,19 @@
+const subjectsData = [
+    {
+        subject: "Mathematics",
+        units: [
+            {
+                name: "Unit 1: Algebra",
+                notes: "https://example.com/algebra_notes.pdf",
+                lectures: [
+                    "https://u.pcloud.link/publink/show?code=XZ9LU55Zh3Spos9PjPSbRNtomDREFkoW9gQy",
+                    "https://u.pcloud.link/publink/show?code=XU2MPZPZfGJyRVEQr4RnH8ZD56V7GZ1VX5V",
+                ],
+                dpps: [
+                    "https://example.com/algebra_dpp1.pdf",
+                    "https://example.com/algebra_dpp2.pdf",
+                ]
+            }
+        ]
+    }
+];
